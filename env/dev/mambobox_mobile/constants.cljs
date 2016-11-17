65fffc37b69b2c29b35f4479c299af0ceece0ced
Juan Monetta <jpmonettas@gmail.com>
2016-11-17 06:34:33 -0300

    Configurations per enviroment

---
+++ env/dev/mambobox_mobile/constants.cljs
@@ -0,0 +1,3 @@
+(ns mambobox-mobile.constants)
+
+(def server-url "http://192.168.1.8:8090")
--- env/prod/mambobox_mobile/constants.cljs
+++ env/prod/mambobox_mobile/constants.cljs
@@ -1,4 +1,3 @@
 (ns mambobox-mobile.constants)
 
-;;(def server-url "http://192.168.1.8:8090")
 (def server-url "http://159.203.142.235:8090")
